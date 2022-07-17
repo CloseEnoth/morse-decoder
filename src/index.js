2fb2b318b190a347d87d55b051fe180327b11c47
CloseEnoth <den.prazdnikov2020@gmail.com>
2022-07-17 23:09:28 +0300

    init: start morse-decoder task

--- src/index.js
+++ src/index.js
@@ -37,9 +37,38 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
+const NUM_MORSE = {
+  '00': '',
+  '10': '.',
+  '11': '-'
 }
+
+function decode(expr) {
+
+  return expr
+  .split('**********') // [ [firstWord], [secondWord]]
+  .map(item => cut(item,10)) //first word -> [ [10Symbols], [same], [same]... ]
+  .map(item => item
+    .map(el => cut(el, 2) //10 symbols -> [ [2symbols], [2symbols], [2symbols], [2symbols]]
+      .map(el => NUM_MORSE[el]) // 2symbols -> '' or '.' or '-'
+      .filter(el => el !== '') // 2symbols clear '' -> '.' or '-'
+      .join(''))// return to 10 symbols
+    .map(item => MORSE_TABLE[item])// 
+    )
+  .map(item => item.join(''))
+  .join(' ')
+}
+
+function cut(arr, num) {
+  let res = []
+
+  for (let i = 0, j = num; i < arr.length; i += num, j += num) {
+    res.push(arr.slice(i, j))
+  }
+
+  return res
+}
+
 
 module.exports = {
     decode
